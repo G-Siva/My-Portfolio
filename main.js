74edd147f75868cfaf901c3a1261d40a678a32fa
G-Siva <111060300+G-Siva@users.noreply.github.com>
2024-07-30 21:45:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+/*const selectElement = (s) => document.querySelector(s);
+const navLinks = document.querySelectorAll(".nav-link");
+
+selectElement(".burger-menu-icon").addEventListener("click", () => {
+    selectElement(".nav-list").classList.toggle("active");
+    selectElement(".burger-menu-icon").classList.toggle("toggle")
+
+    navLinks.forEach((link, index) => {
+        if (link.style.animation){
+            link.style.animation = ""
+        }else{
+            link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
+            console.log(index/7 + 0.5)
+        }
+    })
+});*/
+
+var typed = new Typed('#typing', {
+    strings: ['Siva Subramanian G'],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop: true
+    });
+
+/*navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        selectElement(".nav-list").classList.toggle("active");
+        selectElement(".burger-menu-icon").classList.toggle("toggle");
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = ""
+            }else{
+                link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
+                console.log(index/7 + 0.5)
+            }
+        })
+    })
+})*/
